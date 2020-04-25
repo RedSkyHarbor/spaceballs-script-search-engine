1c05d46a13effd5a25c5ceee1a289f7e8f86478a
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-04-25 18:32:49 -0400

    create model to hold spaceballs quotes

--- spaceballs_search_engine/models.py
+++ spaceballs_search_engine/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+#class Quote(model.Models):
+#    character = models.CharField(max_length=64)
+#    line = models.CharField(max_length=1024)
+#
+#    def __str__(self):
+#        return 'CHARACTER: {}/nLINE: {}'.format(self.character, self.quote)
