2480dcab531f4646ce04f6b556265c80d0143dc4
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-04-25 18:36:06 -0400

    Last commit acidenntally contained necessary code except it was commented out

--- spaceballs_search_engine/models.py
+++ spaceballs_search_engine/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-#class Quote(model.Models):
-#    character = models.CharField(max_length=64)
-#    line = models.CharField(max_length=1024)
-#
-#    def __str__(self):
-#        return 'CHARACTER: {}/nLINE: {}'.format(self.character, self.quote)
+class Quote(model.Models):
+    character = models.CharField(max_length=64)
+    line = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return 'CHARACTER: {}/nLINE: {}'.format(self.character, self.quote)
