cf3ded7e1ea18de00a9d537795aa9cae0372579b
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-04-26 18:52:44 -0400

    Moved form into the header and made selecting characters appear from a dropdown

--- spaceballs_search_engine/forms.py
+++ spaceballs_search_engine/forms.py
@@ -1,5 +1,34 @@
 from django import forms
 
+CHARACTER_CHOICES = [
+    ('helmet', 'Helmet'),
+    ('lone starr', 'Lone Starr'),
+    ('barf', 'Barf'),
+    ('vespa', 'Vespa'),
+    ('dot', 'Dot'),
+    ('skroob', 'Skroob'),
+    ('rico', 'Rico'),
+    ('dinks', 'Dink'),
+    ('sandurz', 'Sandurz'),
+    ('usher', 'Usher'),
+    ('roland', 'Roland'),
+    ('pizza', 'Pizza'),
+    ('vinnie', 'Vinnie'),
+    ('commanderette', 'Comanderette'),
+    ('snotty', 'Snotty'),
+    ('charlene', 'Charlene'),
+    ('marlene', 'Marlene'),
+    ('radar tech.', 'Radar Tech.'),
+    ('corporal', 'Corporal'),
+    ('yogurt', 'Yogurt'),
+    ('pizza guy', 'Pizza Guy'),
+    ('bearded lady', 'Bearded Lady'),
+    ("ship's voice", "Ship's Voice"),
+    ('waitress', 'Waitress'),
+    ('alien', 'Alien'),
+]
+
 class SearchForm(forms.Form):
-    character = forms.CharField(label="Character name", max_length=128, required=False)
-    line = forms.CharField(label="Quote", max_length=1024, required=False)
+    line = forms.CharField(max_length=1024, required=False, label='', widget=forms.TextInput(attrs={'placeholder': 'Quote to search...'}))
+    character = forms.CharField(required=False, label='', widget=forms.Select(choices=CHARACTER_CHOICES))
+
