b8dc5323e4cd880a2b4ae266c564b2ee1580e2f9
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-04-26 02:57:06 -0400

    refactore quote -> line

--- spaceballs_search_engine/forms.py
+++ spaceballs_search_engine/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class SearchForm(forms.Form):
     character = forms.CharField(label="Character name", max_length=128, required=False)
-    quote = forms.CharField(label="Quote", max_length=1024, required=False)
+    line = forms.CharField(label="Quote", max_length=1024, required=False)
