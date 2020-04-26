c07bb231e2e2f3a75f54173626e631cac4da319c
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-04-26 03:04:24 -0400

    Queries db with search parameters supplied from the html form and returns them to the template

--- spaceballs_search_engine/views.py
+++ spaceballs_search_engine/views.py
@@ -5,6 +5,22 @@ from .models import Quote
 
 def homepage(request):
     form = SearchForm(request.POST)
-    all_quotes = Quote.objects.all()
-    context = {'quotes': all_quotes, 'form': form}
+    context = {'form': form}
+
+    # If form was submitted
+    if request.method == 'POST':
+        if form.is_valid():
+            character = form.cleaned_data['character']
+            line = form.cleaned_data['line']
+
+            if line and character:
+                context['quotes'] = Quote.objects.filter(character__iexact=character, line__icontains=line)
+
+            if line:
+                context['quotes'] = Quote.objects.filter(line__icontains=line)
+
+            if character:
+                context['quotes'] = Quote.objects.filter(character__iexact=character)
+
+
     return render(request, 'spaceballs_search_engine/homepage.html', context)
