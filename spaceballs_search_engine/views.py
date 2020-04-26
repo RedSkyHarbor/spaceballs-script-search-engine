158bfc58460526b7bf7281fc16426e0265f215f7
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-04-26 01:41:40 -0400

    refactor all_quotes -> quotes

--- spaceballs_search_engine/views.py
+++ spaceballs_search_engine/views.py
@@ -4,5 +4,5 @@ from .models import Quote
 
 def homepage(request):
     all_quotes = Quote.objects.all()
-    context = {'all_quotes': all_quotes}
+    context = {'quotes': all_quotes}
     return render(request, 'spaceballs_search_engine/homepage.html', context)
