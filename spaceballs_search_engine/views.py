6f6988047f97fc3c29c0d38c2b85ca13008ee12a
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-04-26 02:51:32 -0400

    added search form to homepage

--- spaceballs_search_engine/views.py
+++ spaceballs_search_engine/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
+from .forms import SearchForm
 from .models import Quote
 
 def homepage(request):
+    form = SearchForm(request.POST)
     all_quotes = Quote.objects.all()
-    context = {'quotes': all_quotes}
+    context = {'quotes': all_quotes, 'form': form}
     return render(request, 'spaceballs_search_engine/homepage.html', context)
