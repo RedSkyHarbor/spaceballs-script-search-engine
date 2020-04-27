1dd9d10deb7055b05a5022e24d061db0a4cc38df
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-04-26 20:39:28 -0400

    fixed searching bug

--- spaceballs_search_engine/views.py
+++ spaceballs_search_engine/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from .forms import SearchForm
 from .models import Quote
 
+import json
+
 def homepage(request):
     form = SearchForm(request.POST)
     context = {'form': form, 'first_visit': True}
@@ -16,6 +18,7 @@ def homepage(request):
 
             if line and character:
                 context['quotes'] = Quote.objects.filter(character__iexact=character, line__icontains=line)
+                return render(request, 'spaceballs_search_engine/homepage.html', context)
 
             if line:
                 context['quotes'] = Quote.objects.filter(line__icontains=line)
